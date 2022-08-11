4cfe39a101868b0bdd7c4aa1497ddd0d3cf78dca
Andrea Bettarini <bettarini@monema.it>
2022-08-11 09:54:52 +0200

    First commit

---
+++ README.rst
@@ -0,0 +1,34 @@
+=================
+Contract Sequence
+=================
+
+This module adds a sequence to contracts
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* Andrea Bettarini
+
+Contributors
+~~~~~~~~~~~~
+
+* Andrea Bettarini <bettarini@monema.it>
+
+Other credits
+~~~~~~~~~~~~~
+
+The development of this module has been financially supported by:
+
+* Monema S.r.l.
+
+Maintainers
+~~~~~~~~~~~
+
+This module is maintained by the Monema S.r.l.
+
+.. image:: https://monema.it/img/logo.svg
+   :alt: Monema S.r.l.
+   :target: https://monema.it
---
+++ __manifest__.py
@@ -0,0 +1,54 @@
+
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    jeffery CHEN fan<jeffery9@gmail.com>
+#
+#    Copyright (c) All rights reserved:
+#        (c) 2017  TM_FULLNAME
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses
+#    
+#    Odoo and OpenERP is trademark of Odoo S.A.
+#
+###############################################################################
+{
+    'name': 'Contract Sequence',
+    'summary': 'Contract Sequence Module Project',
+    'version': '14.0.1.0.1',
+
+    'description': """
+Contract Sequence Module Project.
+==============================================
+
+
+    """,
+
+    'author': 'Monema S.r.l.',
+    'maintainer': 'Andrea Bettarini',
+    'contributors': [
+        'Andrea Bettarini <bettarini@monema.it>'
+    ],
+    'website': 'https://monema.it',
+    'license': 'MIT',
+    'category': 'Sales',
+
+    'depends': [
+        'sale',
+        'contract'
+    ],
+    'data': [
+    ],
+    'installable': True
+}
