65abb868c6a5c56228f13ebc05a43a47a365290b
Andrea Bettarini <bettarini@monema.it>
2022-08-13 01:24:19 +0200

    Added tests

---
+++ tests/test_contract_sequence.py
@@ -0,0 +1,29 @@
+from odoo import exceptions
+from odoo.tests import common
+
+class TestContractSequenceBase(common.SingleTransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        admin_user = cls.env.ref('base.user_admin')
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "partner test contract",
+                "email": "demo@demo.com",
+            }
+        )
+
+        cls.contract = cls.env["contract.contract"].create(
+            {
+                "name": "Test Contract",
+                "partner_id": cls.partner.id,
+                "line_recurrence": True,
+            }
+        )
+
+class TestContractSequence(TestContractSequenceBase):
+    def test_contract(self):
+        partner = self.partner.copy()
+        self.contract.partner_id = self.partner.id
+
+        self.assertEqual(self.contract.partner_id, self.partner)
