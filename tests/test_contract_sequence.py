f4c009edb1b63b06c2525a1703a2997f41396b8e
Andrea Bettarini <bettarini@monema.it>
2022-08-13 23:52:37 +0200

    Fix: test_contract

--- tests/test_contract_sequence.py
+++ tests/test_contract_sequence.py
@@ -1,12 +1,10 @@
-from odoo import exceptions
-from odoo.tests import common,tagged
+from odoo.tests import common, tagged
 
 
 class TestContractSequenceBase(common.SingleTransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        admin_user = cls.env.ref('base.user_admin')
         cls.partner = cls.env["res.partner"].create(
             {
                 "name": "partner test contract",
@@ -26,10 +24,8 @@ class TestContractSequenceBase(common.SingleTransactionCase):
 class TestContractSequence(TestContractSequenceBase):
     @tagged('contract_sequence')
     def test_contract(self):
-        partner = self.partner.copy()
         self.contract.partner_id = self.partner.id
-
-        self.assertEqual(self.contract.partner_id, self.partner)
+        self.assertIsNotNone(self.contract.number)
 
     @tagged('contract_sequence')
     def test_contract_copy(self):
