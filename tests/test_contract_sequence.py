beee8e200fe90fa1ca538ffb9e3963848a9d79e5
Andrea Bettarini <bettarini@monema.it>
2022-08-16 23:39:15 +0200

    Fix linter

--- tests/test_contract_sequence.py
+++ tests/test_contract_sequence.py
@@ -38,3 +38,4 @@ class TestContractSequence(TestContractSequenceBase):
     def test_name_get(self):
         contract_name = self.contract.name_get()
         self.assertEqual(contract_name[0][1], "[{}] {}".format(self.contract.number, self.contract.name))
+    
